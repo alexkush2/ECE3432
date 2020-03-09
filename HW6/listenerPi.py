6ee3f6310945cd95a8bcc95538a50720d413889d
Alex <rivershack@live.com>
2020-03-09 12:17:18 -0700

    updated for hardware

--- HW6/listenerPi.py
+++ HW6/listenerPi.py
@@ -9,11 +9,11 @@ def callback(data):
     # duty cycle in range [0.10, 0.20]
 
     steer = (data.axes[0]*.05)+.15
-    #steering.pulse(steer)
+    steering.pulse(steer)
     rospy.loginfo(rospy.get_caller_id() + 'steering: %s, duty cycle %f', data.axes[0], steer)
 
     throttle = (data.axes[1]*.05)+.15
-    #throttle.pulse(throttle)
+    throttle.pulse(throttle)
     rospy.loginfo(rospy.get_caller_id() + 'throttle: %s, duty cycle %f', data.axes[1], throttle)
 
 def listener():
