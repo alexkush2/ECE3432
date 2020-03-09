83da7770f963ffa33147fa0883140a4a3a054e30
Alex <rivershack@live.com>
2020-03-09 08:04:05 -0700

    added HW6 directory, so far single topic control

---
+++ HW6/listenerPi.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Float32
+from rpiHAT import ServoNT
+import time
+# used Float32 as datatype so that it can contain a fractional value for the dutycycle
+
+s=ServoNT(channel=1, freq=94.5)
+t=ServoNT(channel=2, freq=94.5)
+
+def callbackSteer(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard steer %f', data.data)
+    s.pulse(float(data.data))
+
+def callbackThrottle(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard throttle %f', data.data)
+    t.pulse(float(data.data))
+
+
+def listener():
+    
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber('steer', Float32, callbackSteer)
+    rospy.Subscriber('throttle', Float32, callbackThrottle)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    
+    try:
+        rospy.spin()
+    except (KeyboardInterrupt, SystemExit):
+        print "end"
+        s.pulse(0.15)
+        t.pulse(0.15)
+        print "endl"
+
+
+if __name__ == '__main__':
+    listener()
+
+
