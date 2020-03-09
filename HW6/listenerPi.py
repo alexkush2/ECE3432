f69ddd257fea5282819bf0838b905da6b34ff338
Alex <rivershack@live.com>
2020-03-09 12:43:49 -0700

    forgot servo shit

--- HW6/listenerPi.py
+++ HW6/listenerPi.py
@@ -2,24 +2,31 @@
 
 import rospy
 from sensor_msgs.msg import Joy
+from rpiHAT import ServoNT
+import time
 
+s=ServoNT(channel=1, freq=94.5)
+t=ServoNT(channel=2, freq=94.5)
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.axes)
+    rospy.loginfo(rospy.get_caller_id() + ' I heard %s', data.axes)
     # duty cycle in range [0.10, 0.20]
 
     steer = (data.axes[0]*.05)+.15
-    steering.pulse(steer)
-    rospy.loginfo(rospy.get_caller_id() + 'steering: %s, duty cycle %f', data.axes[0], steer)
+    s.pulse(steer)
+    rospy.loginfo(rospy.get_caller_id() + ' steering: %s, duty cycle %f', data.axes[0], steer)
 
     throttle = (data.axes[1]*.05)+.15
-    throttle.pulse(throttle)
-    rospy.loginfo(rospy.get_caller_id() + 'throttle: %s, duty cycle %f', data.axes[1], throttle)
+    t.pulse(throttle)
+    rospy.loginfo(rospy.get_caller_id() + ' throttle: %s, duty cycle %f', data.axes[1], throttle)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
 
     rospy.Subscriber('joy', Joy, callback)
+
+    s.pulse(0.15)
+    t.pulse(0.15)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
