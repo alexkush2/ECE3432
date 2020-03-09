cb3233ae6181a3b194a0e65fb17e929fcc232283
Alex <rivershack@live.com>
2020-03-09 12:46:54 -0700

    servos backwards

--- HW6/listenerPi.py
+++ HW6/listenerPi.py
@@ -12,11 +12,11 @@ def callback(data):
     rospy.loginfo(rospy.get_caller_id() + ' I heard %s', data.axes)
     # duty cycle in range [0.10, 0.20]
 
-    steer = (data.axes[0]*.05)+.15
+    steer = (data.axes[1]*.05)+.15
     s.pulse(steer)
     rospy.loginfo(rospy.get_caller_id() + ' steering: %s, duty cycle %f', data.axes[0], steer)
 
-    throttle = (data.axes[1]*.05)+.15
+    throttle = (data.axes[0]*.05)+.15
     t.pulse(throttle)
     rospy.loginfo(rospy.get_caller_id() + ' throttle: %s, duty cycle %f', data.axes[1], throttle)
 
