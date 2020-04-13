51f7191a694cfbadd9e53d28d30019555b92b12b
alexkush2 <rivershack@live.com>
2020-04-13 18:49:09 -0400

    comments

--- DeepCar/testCar.py
+++ DeepCar/testCar.py
@@ -9,8 +9,10 @@ for i in range(0, sz-1):
     img = cv2.imread(data['image'][i])
     print(data['servo'][i], data['motor'][i])
 
+    # resize
     img = cv2.resize(img, (640,320), interpolation = cv2.INTER_AREA)
 
+    # add line
     steer = data['servo'][i]
     start_point = (150, 300) 
     end_point = (int(steer*1000), 200)
@@ -18,6 +20,7 @@ for i in range(0, sz-1):
     thickness = 9
     img = cv2.line(img, start_point, end_point, color, thickness)
     
+    # add text
     font = cv2.FONT_HERSHEY_SIMPLEX 
     org = (150, 300) 
     fontScale = 1
@@ -27,7 +30,7 @@ for i in range(0, sz-1):
     
     img = cv2.putText(img, motor, org, font,  fontScale, color, thickness, cv2.LINE_AA) 
 
-
+    # show image
     cv2.imshow('image',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
