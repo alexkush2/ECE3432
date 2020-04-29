771b9432859fbdffbec03601340547303d45569b
fuck <fuck@fuck.com>
2020-04-29 16:50:54 -0400

    typos

--- FinalProject/setup.sh
+++ FinalProject/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://github.com/lbaitemple/ubuntu_server_rpi/raw/master/torch/torch-1.6.0a0%2B521910e-cp36-cp36m-linux_armv7l.whl
-sudo pip3 install torch-1.6.0a0%2B521910e-cp36-cp36m-linux_armv7l.whl
+sudo pip3 install torch-1.6.0a0+521910e-cp36-cp36m-linux_armv7l.whl
 
 wget https://github.com/lbaitemple/ubuntu_server_rpi/raw/master/torch/torchvision-0.7.0a0%2Bfed843d-cp36-cp36m-linux_armv7l.whl
-sudo pip3 install torchvision-0.7.0a0%2Bfed843d-cp36-cp36m-linux_armv7l.whl
+sudo pip3 install torchvision-0.7.0a0+fed843d-cp36-cp36m-linux_armv7l.whl
